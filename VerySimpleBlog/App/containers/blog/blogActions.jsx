f9dbf90542cbe51eb4c816a6e0432f76879e87e2
bbolgarev <bbolgarev@national-platform.ru>
2019-10-11 18:02:41 +0300

    add front magic

---
+++ VerySimpleBlog/App/containers/blog/blogActions.jsx
@@ -0,0 +1,16 @@
+﻿import { GET_POSTS_SUCCESS, GET_POSTS_ERROR } from './blogConstants.jsx'
+import "isomorphic-fetch"
+
+export function receivePosts(data) {
+    return {
+        type: GET_POSTS_SUCCESS,
+        posts: data
+    }
+}
+
+export function errorReceive(err) {
+    return {
+        type: GET_POSTS_ERROR,
+        error: err
+    }
+}
